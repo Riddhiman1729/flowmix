4902b0b428e767a99ba68c03989e61554b86e147
Riddhiman1729 <briddhiman1729@gmail.com>
2025-11-10 13:10:59 -0800

    Update dmvnorm.cpp

--- flowmix/src/dmvnorm.cpp
+++ flowmix/src/dmvnorm.cpp
@@ -1,3 +1,4 @@
+
 // [[Rcpp::depends("RcppArmadillo")]]
 #include <RcppArmadillo.h>
 
@@ -6,7 +7,7 @@ static double const log2pi = std::log(2.0 * M_PI);
 /* C++ version of the dtrmv BLAS function */
 void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat){
   arma::uword const n = trimat.n_cols;
-
+  
   for(unsigned j = n; j-- > 0;){
     double tmp(0.);
     for(unsigned i = 0; i <= j; ++i)
@@ -17,28 +18,55 @@ void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat){
 
 // [[Rcpp::export]]
 arma::vec dmvnorm_arma_fast(arma::mat const &x,
-                           arma::rowvec const &mean,
-                           arma::mat const &sigma,
-                           bool const logd = false) {
-    using arma::uword;
-    uword const n = x.n_rows,
-             xdim = x.n_cols;
-    arma::vec out(n);
-    arma::mat const rooti = arma::inv(trimatu(arma::chol(sigma)));
-    double const rootisum = arma::sum(log(rooti.diag())),
-                constants = -(double)xdim/2.0 * log2pi,
-              other_terms = rootisum + constants;
-
-    arma::rowvec z;
-    for (uword i = 0; i < n; i++) {
-        z = (x.row(i) - mean);
-        inplace_tri_mat_mult(z, rooti);
-        out(i) = other_terms - 0.5 * arma::dot(z, z);
-    }
-
-    if (logd)
-      return out;
-    return exp(out);
+                            arma::rowvec const &mean,
+                            arma::mat const &sigma,
+                            bool const logd = false) {
+  using arma::uword;
+  uword const n = x.n_rows,
+    xdim = x.n_cols;
+  arma::vec out(n);
+  arma::mat const rooti = arma::inv(trimatu(arma::chol(sigma)));
+  double const rootisum = arma::sum(log(rooti.diag())),
+    constants = -(double)xdim/2.0 * log2pi,
+    other_terms = rootisum + constants;
+  
+  arma::rowvec z;
+  for (uword i = 0; i < n; i++) {
+    z = (x.row(i) - mean);
+    inplace_tri_mat_mult(z, rooti);
+    out(i) = other_terms - 0.5 * arma::dot(z, z);
+  }
+  
+  if (logd)
+    return out;
+  return exp(out);
 }
 
 // All credit goes to https://gallery.rcpp.org/articles/dmvnorm_arma/
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+*/
