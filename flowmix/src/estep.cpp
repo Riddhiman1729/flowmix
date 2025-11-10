2d8f826bfc7fda44ab8463a87e0c7d53d0bb23b8
Riddhiman1729 <briddhiman1729@gmail.com>
2025-11-10 13:11:32 -0800

    Update estep.cpp

--- flowmix/src/estep.cpp
+++ flowmix/src/estep.cpp
@@ -1,33 +1,42 @@
+// [[Rcpp::depends(RcppEigen)]]
+// Borrow BLAS/LAPACK link flags via this plugin (ok even if you don't use Armadillo types)
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp17)]]
+
+
 #include <numeric>
 #include <RcppArmadillo.h>
+#include <RcppEigen.h>
+
 using namespace Rcpp;
 using namespace arma;
+using namespace Eigen;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 arma::mat estepC(const arma::mat& ylong,
-		 const arma::mat& mnlong,
-		 const arma::vec& sqrt_resp_long,
-		 const double& resp_sum
-		 ){
-
+                 const arma::mat& mnlong,
+                 const arma::vec& sqrt_resp_long,
+                 const double& resp_sum
+){
+  
   arma::mat resid = ylong - mnlong;
   resid.each_col() %= sqrt_resp_long;
   return ( resid.t() * resid ) / resp_sum;
   // return resid;
-
+  
 }
 
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 arma::mat ss(const arma::mat& X,
-	     const arma::uvec& ind) {
-
-              // int n = X.n_rows, k = X.n_cols;
-              // arma::mat X(X.begin(), n, k, false);
-              // arma::uvec ind = as<arma::uvec>(ind_);
-              arma::mat submat = X.rows(ind - 1);
-              // return wrap(submat);
-	      return submat;
+             const arma::uvec& ind) {
+  
+  // int n = X.n_rows, k = X.n_cols;
+  // arma::mat X(X.begin(), n, k, false);
+  // arma::uvec ind = as<arma::uvec>(ind_);
+  arma::mat submat = X.rows(ind - 1);
+  // return wrap(submat);
+  return submat;
 }
